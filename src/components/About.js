08cfb714ff19979b4a99ebd30bc4649a6da023a4
unknown <laura.feregotti@gmail.com>
2019-05-14 14:24:29 +0200

    routing tutorial 25-26-27

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div className="container">
+         <h4 className="center"> About</h4>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non voluptates, iusto quisquam aperiam possimus saepe nemo magni odio, inventore ab magnam illo, quaerat cupiditate quas eligendi? Itaque, exercitationem in? Esse?</p>
+
+        </div>
+    )
+}
+
+export default About
